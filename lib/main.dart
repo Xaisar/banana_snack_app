799dcf9bc9895e9422635bcab6c4edcae370c285
aji_hermansya <ajihermansya14@gmail.com>
2023-10-06 14:54:07 +0700

    pubspec-icon

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,42 @@
+import 'package:banana_snack_application/widgets/splashscreen.dart';
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
 
 import 'app/routes/app_pages.dart';
 
-void main() {
+void main () {
+  runApp (MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    // return GetMaterialApp(
+    //   debugShowCheckedModeBanner: false,
+    //   home: HomePetugasView(),
+    // );
+    return FutureBuilder(
+      future: Future.delayed(Duration(seconds: 3)),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return SplashScreen();
+        } else {
+          return GetMaterialApp(
+            debugShowCheckedModeBanner: false,
+            title: "Bank Sampah Digital",
+            initialRoute: AppPages.INITIAL,
+            getPages: AppPages.routes,
+          );
+        }
+      },
+    );
+  }
+}
+
+
+/*void main() {
   runApp(
     GetMaterialApp(
       title: "Application",
@@ -12,4 +44,5 @@ void main() {
       getPages: AppPages.routes,
     ),
   );
-}
+}*/
+
