6a2f10ce5a355bcebbe6f1dd44fd21746a5bf075
aji_hermansya <ajihermansya14@gmail.com>
2023-10-11 07:46:21 +0700

    splash-screen-done

--- lib/widgets/splashscreen.dart
+++ lib/widgets/splashscreen.dart
@@ -1,4 +1,6 @@
+import 'package:banana_snack_application/app/modules/home/views/home_view.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -7,7 +9,29 @@ class SplashScreen extends StatefulWidget {
   State<SplashScreen> createState() => _SplashScreenState();
 }
 
-class _SplashScreenState extends State<SplashScreen> {
+class _SplashScreenState extends State<SplashScreen> 
+with SingleTickerProviderStateMixin{
+    @override
+    void initState(){
+      super.initState();
+      SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersive);
+
+      Future.delayed(Duration(seconds: 2),(){
+        Navigator.of(context).pushReplacement(MaterialPageRoute(
+          builder: (_) => const HomeView(),
+        ),
+        );
+      });
+    }
+
+    @override
+  void dispose() {
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual, 
+    overlays: SystemUiOverlay.values);
+    super.dispose();
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return  Scaffold(
