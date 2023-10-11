9fae72755f24aeae1dc5b07b8586a6a1f9ddcf11
aji_hermansya <ajihermansya14@gmail.com>
2023-10-11 09:01:30 +0700

    splashscreen-new image

--- lib/widgets/splashscreen.dart
+++ lib/widgets/splashscreen.dart
@@ -1,6 +1,7 @@
 import 'package:banana_snack_application/app/modules/home/views/home_view.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:get/get.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -38,19 +39,57 @@ with SingleTickerProviderStateMixin{
       body: Container(
         width: double.infinity,
         decoration: BoxDecoration(
-        gradient: LinearGradient(colors: [Colors.blue, Colors.purple],
+        gradient: LinearGradient(colors: [ Colors.white, Color.fromARGB(500, 252, 206, 47)],
         begin: Alignment.bottomLeft,
-        ), 
+        ),
         ),
 
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Icon(Icons.edit,
-            size:80,
-            color: Colors.white,)
+
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                SizedBox(
+                  width: Get.width * 0.6,
+                  height: Get.width * 0.6,
+                  child: Image.asset("assets/logo/logo.png"),
+                ),
+              ],
+            ),
+
+             Container(
+              child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Padding(padding: const EdgeInsets.only(bottom: 10),
+                child: Text("Obugame",
+                textAlign: TextAlign.center,
+                style: TextStyle(color: Colors.black,
+                fontSize: 20,
+                fontWeight: FontWeight.bold),
+                ),
+                ),
+              ],
+              ),
+            )
+
+           
           ],
+          
         ),
+
+
+        
+
+      
+
+        
+
+
 
 
       ),
